5d1570094b198f559fb5f69e42f8db96e6cbb8af
kidbai <ybooty0@gmail.com>
2017-04-27 17:04:43 +0800

    生成一个期望的json格式

--- index.py
+++ index.py
@@ -1,25 +1,32 @@
 #-*- coding:utf-8 -*-
 import json
+import codecs
 data = {}
-f = open('./province-city.txt')
+f = codecs.open('./test.txt', 'r', 'utf-8')
 lines = f.readlines()
 f.close()
-output = open('./output.json', 'w')
+output = codecs.open('./output.json', 'w', 'utf-8')
 for line in lines:
     if not line:
         break
     else:
         arr = line.split()
+        print arr[1]
         if arr[0].isdigit():
-            print arr[1]
-            print data.has_key(arr[1])
-            data[arr[1]] = {
-                "code": arr[0],
-                "city": arr[2]
-            }
+            if data.has_key(arr[1]):
+                data[arr[1]]['city'][arr[2]] = []
+            else:
+                data[arr[1]] = {
+                    "code": arr[0],
+                    "city": {
+                        arr[2]: []
+                    }
+                }
         else:
             print 'not valid line'
 
+for k,v in data.items():
+    print k,v['code']
 output.write(json.dumps(data, indent=4, sort_keys=True))
 output.close()
 
